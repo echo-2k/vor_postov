16fa02b700aed5ec3c0b3fcc9a41a31079ec6ad7
echo-2k <echo2k@internet.ru>
2024-07-21 21:14:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 from telethon import TelegramClient, events
 from aiogram import Bot, Dispatcher, types, executor
 import asyncio
+import re
 
-api_id = 1234567890 # Получить https://my.telegram.org/auth
+api_id = 1234567890  # Получить https://my.telegram.org/auth
 api_hash = '0123456789abcdef0123456789abcdef'
-source_channels = [-1234567890, -1234567890] # откуда берём посты
+source_channels = [-1234567890, -1234567890]  # откуда берём посты
 destination_channels = {
     -1234567890: "<b><a href='https://t.me/savage1262'>🔪 Savage1262</a></b>",
     -1234567890: "<b><a href='https://t.me/savage1262'>🔪 Savage1262</a></b>"
-} # ID основного канала: Описание(можно использовать несколько каналов)
+}  # ID основного канала: Описание(можно использовать несколько каналов)
 
-bot_token = '122'  # Токен бота
-log_chat_id =   # ID чата, куда будут отправляться посты
+bot_token = '122'  # Токен бота для логов
+log_chat_id = -1234567890  # ID чата, куда будут отправляться логи
 stop_words = ['https', 'казино', 'ставки', 'пока не удалили', 'игра', 'крипта', '@', '.', 'послед', '-']  # Заменить на другие исключения
 
 client = TelegramClient('vor_postov', api_id, api_hash)
-
 bot = Bot(token=bot_token, parse_mode='HTML')
 dp = Dispatcher(bot)
 
 async def start_telegram_client():
     await client.start()
-
     try:
         for channel in source_channels:
             access = await client.get_entity(channel)
@@ -35,18 +34,20 @@ async def start_telegram_client():
         await bot.send_message(log_chat_id, f'❌ Ошибка: {e}')
         return
 
-@client.on(events.Album(chats=source_channels)) 
+def remove_links(text):
+    return re.sub(r'http\S+|www\S+|@\S+', '', text)
+
+@client.on(events.Album(chats=source_channels))
 async def new_album(event):
-    
     original_message = event.original_update.message.message
+    clean_message = remove_links(original_message)
 
     # Проверяем наличие запрещенных слов в сообщении
-    if any(stop_word in original_message for stop_word in stop_words):
-        log_message = f"<i><b>♻️ Сообщение содержит запрещённые слова.\n\n</b></i><code>{original_message}</code>"
-        
+    if any(stop_word in clean_message for stop_word in stop_words):
+        log_message = f"<i><b>♻️ Сообщение содержит запрещённые слова.\n\n</b></i><code>{clean_message}</code>"
         await bot.send_message(log_chat_id, log_message)
         return
-    
+
     for channel_id, description in destination_channels.items():
         await client.send_message(
             entity=channel_id,
@@ -58,26 +59,24 @@ async def new_album(event):
 
 @client.on(events.NewMessage(chats=source_channels))
 async def handler(event):
-
     original_message = event.message.message
+    clean_message = remove_links(original_message)
 
     # Проверяем наличие запрещенных слов в сообщении
-    if any(stop_word in original_message for stop_word in stop_words):
-        log_message = f"<i><b>♻️ Сообщение содержит запреты.\n\n</b></i><code>{original_message}</code>"
-        
+    if any(stop_word in clean_message for stop_word in stop_words):
+        log_message = f"<i><b>♻️ Сообщение содержит запреты.\n\n</b></i><code>{clean_message}</code>"
         await bot.send_message(log_chat_id, log_message)
         return
 
     if event.message.grouped_id:
         pass
-
     else:
         if event.message.media:
             for channel_id, description in destination_channels.items():
                 await client.send_file(
-                    channel_id, 
-                    event.message.media, 
-                    caption = description,
+                    channel_id,
+                    event.message.media,
+                    caption=description,
                     parse_mode='html'
                 )
             await bot.send_message(log_chat_id, f"<b>✅ Сообщение успешно отправлено!</b>")
@@ -85,7 +84,7 @@ async def handler(event):
             for channel_id, description in destination_channels.items():
                 await client.send_message(channel_id, description, parse_mode='html')
             await bot.send_message(log_chat_id, f"<b>✅ Сообщение успешно отправлено!</b>")
-    
+
 async def main():
     await start_telegram_client()
     await dp.start_polling()
